2377111d87b3a0d9df72da7d85bdf5e007de5a13
VainerV <vainerv@yahoo.com>
2018-04-13 21:41:02 -0700

    New functions addes to JS file, Minor update to HTML and CSS

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
+const STATE = {
+  enableField: true,
+  zipCode: ""
+};
+
+
+ 
 $(document).ready(function(){
+    enableListeners();
+    render();
+});
+
+
+// Main render function
+function render() {
+    onOffSearchField();
+    console.log("my state" , STATE);
+  }
+
+// Enables all listeners (controler)
+function enableListeners(){
+    storeZipCode();
+    submitZipCode();
+    
+}
+
+
+// Enable search fields (view)
+function onOffSearchField() {
+  $("#single-search").prop("disabled", STATE.enableField);
+}
+
+// Changing State Enable fiels from false to true and oposite (controler);
+function toggleSearchField() {
+  STATE.enableField = !STATE.enableField;
+  render();
+}
+
+
+
+function submitZipCode() {
+  // enable restarant field and submit zipcode
+  $("#submit-location-search").on("click", event => {
+    event.preventDefault();
+    toggleSearchField();
+  });
+  
+}
+
+// Stores entered zipcode(controller)
+function storeZipCode(){ 
+    $("#zipcode-search").change( (event) => {
+     updateZipCodeValue(event.target.value);
+  });
+}
+
+// Update STATE zipcode Value
+function updateZipCodeValue(zipCodeValue){
+    STATE.zipCode = zipCodeValue;
+    render();
+}
+
+
+
+
+/*$(document).ready(function(){
 
     $("#submit-location-search").on("click",(event) => {
         event.preventDefault();
